2f4a5bf562dc64958f1651bdc0c27eea428a6494
Sefanya Yemima Sinaga <114148492+sefanyas123@users.noreply.github.com>
2024-05-07 15:42:50 +0700

    pair 4

--- src/academic/model/Course.java
+++ src/academic/model/Course.java
@@ -1,11 +1,40 @@
 package academic.model;
 
+import java.util.List;
+
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S22051 SEFANYA YEMIMA SINAGA
  */
-public class Course {
+public class Course extends Inheritence{
 
-    // class definition
+    //DEKLARASI VARIABEL CODE NAME PASSING GRADE KREDIT
 
-}
+    private String passingGrade;
+    private int credit;
+    private List<String> lecturerInitialList;
+
+
+    //konstructor
+    public Course(String code, String name, int credit, String passingGrade) {
+        super(name, null, null, null, null,null, null, null,code,null);  
+        this.credit = credit;
+        this.passingGrade = passingGrade;
+  
+    }
+    //getter
+ 
+
+    //getter
+    public List <String> getlecturerInitialList(){
+        return lecturerInitialList; 
+    }
+
+    public String getPassingGrade(){
+        return passingGrade;
+    }
+
+    public int getCredit(){
+        return credit; 
+    }    
+
+} 
