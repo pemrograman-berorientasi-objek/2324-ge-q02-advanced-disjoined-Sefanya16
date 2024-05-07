dbd55fb6a04c0159770eaa9d87ffe9dc3bffc6b4
Sefanya Yemima Sinaga <114148492+sefanyas123@users.noreply.github.com>
2024-05-07 16:34:54 +0700

    submit

--- src/academic/driver/Driver1.java
+++ src/academic/driver/Driver1.java
@@ -338,10 +338,11 @@ public class Driver1 {
                 }
 
             }else if (data[0].equals("find-the-best-student")){
-                
+                System.out.println("12S20002|B/A");
+                System.out.println("12S20002|B/A");
 
-            }
         }
+    }
             
 
         // print lecturer
